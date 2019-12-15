cdb93981600fbdaba6d68ebf0438d1043015b344
MegY <meghdadyazdi@yahoo.com>
2019-12-15 13:55:46 +0000

    Keys added

--- js/Tetris_jQuery.js
+++ js/Tetris_jQuery.js
@@ -3,80 +3,78 @@ $(document).ready(function () {
     $(document).keydown(function (e) {
         if (e.which == 37) {
             Tetrimos.moveLeft();
-            if (collision(board, Tetrimos)){
+            if (collision(board, Tetrimos)) {
                 Tetrimos.moveRight();
             }
         }
         else if ((e.keyCode == 38)) {
             Tetrimos.rotateClockwise();
-            if (collision(board, Tetrimos)){
+            if (collision(board, Tetrimos)) {
                 Tetrimos.rotateCounterClockwise();
             }
         }
         else if ((e.keyCode == 17)) {
             Tetrimos.rotateCounterClockwise();
-            if (collision(board, Tetrimos)){
+            if (collision(board, Tetrimos)) {
                 Tetrimos.rotateCounter();
             }
         }
         else if (e.keyCode == 39) {
             Tetrimos.moveRight();
-            if (collision(board, Tetrimos)){
+            if (collision(board, Tetrimos)) {
                 Tetrimos.moveLeft();
             }
         }
         else if ((e.keyCode == 40)) {
             Tetrimos.moveDown();
-            if (collision(board, Tetrimos)){
+            if (collision(board, Tetrimos)) {
                 Tetrimos.moveUp();
             }
         }
         else if ((e.keyCode == 32)) {
-            if (gameStatus){
-                gameStatus=false;
-            }else{
-                gameStatus=true;
+            if (gameStatus) {
+                gameStatus = false;
+            } else {
+                gameStatus = true;
             }
-            
+
         }
-        
-    });
-    // $("#score").text(String(score));
 
-$("#leftbtn").click(function () {
-    Tetrimos.moveLeft();
-            if (collision(board, Tetrimos)){
-                Tetrimos.moveRight();
-            }
+    });
+    $("#leftarrow").click(function () {
+        Tetrimos.moveLeft();
+        if (collision(board, Tetrimos)) {
+            Tetrimos.moveRight();
+        }
     });
 
-    $("#rotatebtn").click(function () {
+    $("#rotatearrow").click(function () {
         Tetrimos.rotateClockwise();
-            if (collision(board, Tetrimos)){
-                Tetrimos.rotateCounterClockwise();
-            }    
+        if (collision(board, Tetrimos)) {
+            Tetrimos.rotateCounterClockwise();
+        }
     });
 
-    $("#rightbtn").click(function () {
+    $("#rightarrow").click(function () {
         Tetrimos.moveRight();
-            if (collision(board, Tetrimos)){
-                Tetrimos.moveLeft();
-            }    
+        if (collision(board, Tetrimos)) {
+            Tetrimos.moveLeft();
+        }
     });
 
-    $("#downbtn").click(function () {
+    $("#downarrow").click(function () {
         Tetrimos.moveDown();
-            if (collision(board, Tetrimos)){
-                Tetrimos.moveUp();
-            }    
+        if (collision(board, Tetrimos)) {
+            Tetrimos.moveUp();
+        }
     });
 
     $("#inner-circle").click(function () {
-         if (gameStatus){
-                gameStatus=false;
-            }else{
-                gameStatus=true;
-            }
+        if (gameStatus) {
+            gameStatus = false;
+        } else {
+            gameStatus = true;
+        }
     });
 
 
